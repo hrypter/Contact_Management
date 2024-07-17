f904259d36be5ac544de5314d5d5015471291a1b
SUMAN KUMAR CHAND <161135239+hrypter@users.noreply.github.com>
2024-07-17 13:54:39 +0000

    test

---
+++ Contact-management/src/main/java/in/contact/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package in.contact.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers("/contact/edit/{id}","/contact/delete/{id}","/contact/new","/favorite/**").permitAll() // Permit access to your endpoints
+                                .anyRequest().authenticated() // Secure other endpoints
+                )
+                .formLogin(withDefaults())
+                .csrf(csrf -> csrf.disable()); // Disable CSRF for simplicity (consider enabling it for better security)
+        return http.build();
+    }
+}
+
